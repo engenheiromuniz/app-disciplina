df4976869f9382ea90bf6205e77408503593f841
Engenheiro Muniz <engenheiro.muniz@outlook.com>
2026-07-09 16:23:19 -0300

    Created class src/app/disciplina.service.ts

--- disciplina-frontend/app-disciplina/src/app/disciplina.service.ts
+++ disciplina-frontend/app-disciplina/src/app/disciplina.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Disciplina } from './models/disciplina.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DisciplinaService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/disciplinas';
+
+  constructor(private http: HttpClient) { }
+
+  listar(): Observable<Disciplina[]> {
+    return this.http.get<Disciplina[]>(this.apiUrl);
+  }
+
+  salvar(disciplina: Disciplina): Observable<Disciplina> {
+    return this.http.post<Disciplina>(this.apiUrl, disciplina);
+  }
+
+  atualizar(id: number, disciplina: Disciplina): Observable<Disciplina> {
+    return this.http.put<Disciplina>(`${this.apiUrl}/${id}`, disciplina);
+  }
+
+  deletar(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+  
 }
