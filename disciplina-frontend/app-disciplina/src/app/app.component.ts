506e8965264e40f47996e15764828c547ecb2d9d
Engenheiro Muniz <engenheiro.muniz@outlook.com>
2026-07-09 16:52:21 -0300

    Created and improved the views form-fisciplina.component.html and liata-sidiplina.componente.html and finally, was improved too the ap.componente.htaml and app.componente.ts

--- disciplina-frontend/app-disciplina/src/app/app.component.ts
+++ disciplina-frontend/app-disciplina/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaDisciplinaComponent } from './pages/lista-disciplina/lista-disciplina.component';
+import { FormDisciplinaComponent } from './pages/form-disciplina/form-disciplina.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListaDisciplinaComponent, FormDisciplinaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
