200905fd77f347c261787d9580ee1a42a4f1f146
Engenheiro Muniz <engenheiro.muniz@outlook.com>
2026-07-09 16:33:41 -0300

    Created class FormDisciplinaComponent

--- disciplina-frontend/app-disciplina/src/app/pages/form-disciplina/form-disciplina.component.ts
+++ disciplina-frontend/app-disciplina/src/app/pages/form-disciplina/form-disciplina.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DisciplinaService } from '../../disciplina.service';
+import { Disciplina } from '../../models/disciplina.model';
 
 @Component({
   selector: 'app-form-disciplina',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './form-disciplina.component.html',
-  styleUrl: './form-disciplina.component.scss'
+  styleUrls: ['./form-disciplina.component.scss']
 })
 export class FormDisciplinaComponent {
+  disciplina: Disciplina = { nome: '', descricao: '' };
 
+  constructor(private disciplinaService: DisciplinaService) {}
+
+  salvar() {
+    this.disciplinaService.salvar(this.disciplina).subscribe({
+      next: () => {
+        alert('Disciplina cadastrada com sucesso!');
+        this.disciplina = { nome: '', descricao: '' };
+      },
+      error: (err) => {
+        console.error('Erro ao salvar disciplina', err);
+        alert('Erro ao salvar disciplina.');
+      }
+    });
+  }
 }
--- disciplina-frontend/app-disciplina/src/app/pages/lista-disciplina/lista-disciplina.component.ts
+++ disciplina-frontend/app-disciplina/src/app/pages/lista-disciplina/lista-disciplina.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DisciplinaService } from '../../disciplina.service';
+import { Disciplina } from '../../models/disciplina.model';
 
 @Component({
   selector: 'app-lista-disciplina',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './lista-disciplina.component.html',
   styleUrl: './lista-disciplina.component.scss'
 })
-export class ListaDisciplinaComponent {
+export class ListaDisciplinaComponent implements OnInit{
+  disciplinas: Disciplina[] = [];
+
+  constructor(private disciplinaService: DisciplinaService){}
+
+  ngOnInit(): void {
+    this.disciplinaService.listar().subscribe({
+      next: (dados) => this.disciplinas = dados,
+      error: (err) => console.error('Erro ao listar disciplina',err)
+    });
+  }
 
 }
